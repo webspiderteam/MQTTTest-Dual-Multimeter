6ae9e17c89568ca2021fc9d2a08214ca2d388160
WebSpiderTeam <webspiderteam@hotmail.com>
2025-02-03 21:55:28 +0300

    fix namespace

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace MQTTTest2
+namespace MQTTTest
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MQTTTest2")]
+[assembly: AssemblyTitle("MQTTTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MQTTTest2")]
+[assembly: AssemblyProduct("MQTTTest")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
